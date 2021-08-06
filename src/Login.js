c2caca5c5ed5dbed616570d4cb99d32099fd34b9
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2021-08-06 06:06:59 +0100

    Spotify like App - start

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.css';
+
+const Login = props => {
+    return (
+        <div className="login">
+
+            <img src="https://community.spotify.com/t5/image/serverpage/image-id/27289i6CB37DCE3D36368E/image-size/original?v=mpbl-1&px=-1" alt="" />
+            <a>LOGIN WITH SPOTUFY</a>
+        </div>
+
+    )
+}
+
+export default Login
