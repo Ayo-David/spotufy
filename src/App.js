d5c00b96e5349540b3708222e56afb48d032b464
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2021-08-06 13:22:57 +0100

    just a little update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './Login';
+import { getTokenFromUrl } from './spotufy';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi;
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+
+    const hash = getTokenFromUrl();
+
+    window.location.hash = "";
+    const _token = hash.access_token;
+    console.log("I have a token 😃 ", token);
+
+    if (_token) {
+      setToken(_token)
+      // spotify.setAccessToken(_token);
+      // spotify.getMe().then(user => {
+      //   console.log("👨", user);
+      // })
+    }
+
+
+  }, [])
+
   return (
     <div className="App">
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
 
-      <Login />
     </div>
 
   );
